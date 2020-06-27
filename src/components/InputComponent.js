c19505914aab0c923ab01c6b664d8a9760b8bc40
Yazan <yazanmatar90@gmail.com>
2020-06-27 01:18:23 -0400

    input initial

---
+++ src/components/InputComponent.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const InputComponent = ({ handleChange }) => {
+  return (
+    <div style={styles.container}>
+      <input
+        style={styles.input}
+        placeholder="Search by name"
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+const styles = {
+  input: {
+    width: "95%",
+    height: "40px",
+    border: "0px",
+    borderBottom: "1px solid rgba(0, 0, 0, .2)",
+    fontSize: "20px",
+  },
+  container: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+  },
+};
+export default InputComponent;
