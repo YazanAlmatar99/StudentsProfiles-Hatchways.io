d2947b57d1d41c541ae6ea5588b321d5f5e3404e
Yazan <yazanmatar90@gmail.com>
2020-06-27 01:18:59 -0400

    added helper functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import profiles from "./API/profiles";
+import ProfileCard from "./components/ProfileCard";
+import InputComponent from "./components/InputComponent";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    profiles: [],
+    renderedProfiles: [],
+    profilesToShow: [],
+    nameTerm: "",
+  };
+  componentWillMount() {
+    this.handleData();
+  }
+  handleChange = async (e) => {
+    await this.setState({ nameTerm: e.target.value });
+    console.log(this.state.nameTerm.length, "name term");
+    const profilesToShow = this.state.renderedProfiles.filter((profile) => {
+      return profile.props.profile.firstName
+        .toLowerCase()
+        .includes(this.state.nameTerm);
+    });
+    this.setState({ profilesToShow: profilesToShow });
+  };
+  mapArr = () => {
+    const mappedArr = this.state.profiles.map((profile) => {
+      return <ProfileCard profile={profile} key={profile.firstName} />;
+    });
+    this.setState({ renderedProfiles: mappedArr });
+    return mappedArr;
+  };
+
+  handleData = async () => {
+    const response = await profiles.get("/");
+    this.setState({ profiles: response.data.students });
+    this.mapArr();
+  };
+  render() {
+    if (this.state.profiles) {
+      return (
+        <div>
+          <InputComponent handleChange={this.handleChange} />
+          {this.state.profilesToShow.length
+            ? this.state.profilesToShow
+            : this.state.renderedProfiles}
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 export default App;
